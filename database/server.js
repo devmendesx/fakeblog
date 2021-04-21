0fbd17a0c80b87269e7e2984eeaf3f85acd09d50
Matheus Mendes <matheushenriquesad@gmail.com>
2021-04-20 22:02:33 -0300

    utc hours

--- database/server.js
+++ database/server.js
@@ -4,6 +4,10 @@ const connection = new Sequelize("blog", "root", "admin", {
   host: "raisbbsdb",
   dialect: "mysql",
   timezone: "-03:00",
+  dialectOptions: {
+    useUTC: true,
+    timezone: "-03:00",
+  },
 });
 
 module.exports = connection
--- database/user.js
+++ database/user.js
@@ -3,6 +3,10 @@ const connection = new Sequelize("users", "root", "admin", {
   host: "raisbbsdb",
   dialect: "mysql",
   timezone: "-03:00",
+  dialectOptions: {
+    useUTC: true,
+    timezone: "-03:00",
+  },
 });
 
 module.exports = connection;
